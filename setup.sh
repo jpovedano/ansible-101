f67b3e2b01d60fb19cedcc2daa4126fc9de4e95e
Javier Povedano-Molina <javier@rti.com>
2023-05-02 11:47:35 +0200

    Update setup

--- setup.sh
+++ setup.sh
@@ -1,7 +1,12 @@
 #!/usr/bin/bash
 
+# Create a virtual environment to not pollude our
+# system-wide python installation
 python3 -m venv .venv
 . .venv/bin/activate
+
+# Install the requirements for this tutorial
 pip install -r requirements.txt
 
+# Install additional ansible dependencies for this tuturial
 ansible-galaxy collection install community.libvirt
